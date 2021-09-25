47f2c4f66715727e5cd6832d5a385c5ffeb5e811
Ourai Lin <ourairyu@gmail.com>
2021-09-25 15:38:43 +0800

    feat(view-ui): `Icon` component

---
+++ packages/core/src/icon/Icon.ts
@@ -0,0 +1,14 @@
+import { IIconComponent, IconHeadlessComponent } from 'petals-ui/dist/icon';
+import { Component, Prop } from 'vue-property-decorator';
+
+import { BaseStructuralComponent } from '../basic';
+
+@Component
+class IconStructuralComponent
+  extends BaseStructuralComponent<IconHeadlessComponent>
+  implements IIconComponent {
+  @Prop({ type: String, default: '' })
+  public readonly refs!: string;
+}
+
+export { IconStructuralComponent };
---
+++ packages/view-ui/src/icon/logic.ts
@@ -0,0 +1,18 @@
+import { CreateElement, VNode } from 'vue';
+import { Component } from 'vue-property-decorator';
+
+import { IconStructuralComponent } from '@kokiri/core/dist/icon';
+import { Icon as IvuIcon } from 'view-design';
+
+import { getComponentName } from '../basic';
+
+@Component({
+  // @ts-ignore
+  abstract: true,
+  name: getComponentName('icon'),
+})
+export default class Icon extends IconStructuralComponent {
+  private render(h: CreateElement): VNode {
+    return h(IvuIcon, { props: { type: this.refs } }, this.$slots.default);
+  }
+}
