34df4306183e0ed628c58cb682080b16cc2e0adc
Ourai Lin <ourairyu@gmail.com>
2021-10-22 22:52:10 +0800

    fix(view-ui): max width and word wrap of `Tooltip` component

--- packages/view-ui/src/tooltip/Tooltip.vue
+++ packages/view-ui/src/tooltip/Tooltip.vue
@@ -1,10 +1,11 @@
 <template>
   <ivu-tooltip
-    :class="className"
+    :class="['Tooltip', className]"
     :content="content"
     :placement="resolvedPlacement"
     :disabled="disabled"
     :transfer="appendToBody"
+    transfer-class-name="Tooltip-transfer"
   >
     <slot />
     <slot name="content" slot="content" />
@@ -12,3 +13,13 @@
 </template>
 
 <script lang="ts" src="./logic.ts"></script>
+
+<style src="./style.scss" lang="scss" scoped></style>
+
+<style lang="scss">
+.Tooltip-transfer .ivu-tooltip-inner {
+  @include line_feed;
+
+  max-width: none;
+}
+</style>
